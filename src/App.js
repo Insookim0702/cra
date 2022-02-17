b4a10309f8b4a964183a405ed5742c2bb51d409f
Insoo Kim <42515875+Insookim0702@users.noreply.github.com>
2022-02-17 23:19:34 +0900

    - 필요 없는 코드 삭제 - 전역 헤더 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./index.css";
+import Header from "./global/header";
 function App() {
-    return <>hhee</>;
+    return (
+        <>
+            <Header prop="123" />
+            hhee
+        </>
+    );
 }
 
 export default App;
